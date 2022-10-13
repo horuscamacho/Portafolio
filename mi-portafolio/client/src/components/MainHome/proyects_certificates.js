86a0149e04436c9ce29352f5fde975062bf63ccf
Horus Sinhue <camachohorus@gmail.com>
2022-10-13 09:39:25 -0500

    Servidor del back listo

--- mi-portafolio/client/src/components/MainHome/proyects_certificates.js
+++ mi-portafolio/client/src/components/MainHome/proyects_certificates.js
@@ -5,3 +5,6 @@ export const proyectos = [
 export const cursos = [
     ["Cursos y certificaciones"], [{id: 1, curso: "Curso Práctico de Maquetación en CSS", descripcion: "Crear un espacio personal para reforzar conocimientos en maquetación de HTML5 semántico, maquetando el header, el main y el footer, creando estilos para el proyecto de forma estética", link: "https://i.postimg.cc/VkszpFt4/diploma-practico-css.png" }, {id: 2, curso: "Resp Design: Maquetación Mobile First", descripcion: "Aprender a crear prouectos que se adaptarán a las distintas vistas que utilicen los usuarios, se analizó la arquitectura de un proyecto partiendo de un wireframe, auditado, con lighthouse. ", link: "https://i.postimg.cc/k42DVPv2/diploma-mobile-first-1.png"}, {id: 3, curso: "EF SET Certificate C1 Advanced", descripcion: "The EF Standard English Test (EF SET) is the world´s first completely free, online standardized English test for learners of all levels, from beginner to advanced. As part of EF Education First, we draw upon a 50-year legacy", link: 'https://i.postimg.cc/52SWTD9Y/Captura-de-Pantalla-2022-10-12-a-la-s-15-34-45.png',}]
   ]
+
+
+  
