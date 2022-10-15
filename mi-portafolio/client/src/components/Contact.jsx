88dbef22b38c976ff2f714b7fb44ce86b8eb9b75
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2022-10-15 10:18:53 -0500

    Arreglando el teléfono

--- mi-portafolio/client/src/components/Contact.jsx
+++ mi-portafolio/client/src/components/Contact.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import ContactSections from './ContactSections'
+import SectionMain from './MainContact/SectionMain'
 const Contact = () => {
   return (
     <>
         <Header/>
+        <SectionMain />
         <ContactSections />
         <Footer />
     </>
